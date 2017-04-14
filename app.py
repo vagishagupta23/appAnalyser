b2b3460b1536adcbd12ee4e3c9e51850ccba4622
Vagisha Gupta <vagishagupta23@gmail.com>
2017-04-14 18:52:37 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+from scrape3 import *
 app = Flask(__name__)
 
 @app.route("/")
@@ -8,15 +9,30 @@ def main():
 @app.route('/getStarted')
 def starting():
     return render_template('started.html')
-   
-@app.route('/appSearch')
+
+@app.route('/appSearch', methods=['POST','GET'])
 def appsearch():
-    return render_template('search.html')
+   if request.method == 'GET':
+      return render_template('search.html')
+   elif request.method == 'POST':
+      return redirect(url_for('search_result'))
+
+@app.route('/searchResult', methods=['POST','GET'])
+def search_result():
+   search_tb= request.form['sr']
+   res,result=search(search_tb)
+   res=app_details(result)
+   return render_template('searchResult.html', name=res) 
+   
 
 
 @app.route('/similarApp')
 def appsimilar():
-    return render_template('similar.html')   
+    return render_template('similar.html')
+
+@app.route('/appCompare')
+def comparing():
+    return render_template('compare.html')   
 
 if __name__ == '__main__':
-   app.run(debug = True)
+   app.run()
