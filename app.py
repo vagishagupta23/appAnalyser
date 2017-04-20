ce935c50e8eab7d6822f003e7f50aa19faa199b6
Vagisha Gupta <vagishagupta23@gmail.com>
2017-04-20 22:48:31 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
-from flask import Flask, render_template, request, redirect
-from scrape3 import *
+from flask import Flask, render_template, request, redirect, session, url_for
+from flask_images import resized_img_src
+from PIL import Image
+import scrape3
 app = Flask(__name__)
+app.secret_key="fuck up"
+images = Images(app)
 
 @app.route("/")
 def main():
@@ -16,20 +20,39 @@ def appsearch():
       return render_template('search.html')
    elif request.method == 'POST':
       search_tb= request.form['sr']
-      res,result=search(search_tb)
+      session['app_name']=search_tb
+      #print search_tb
+      #res,result=scrape3.search(search_tb)
       #print res
-      res=app_details(result)
-      return render_template('searchResult.html', name=res)
-      #return redirect(url_for('search_result'))
+      #res=scrape3.app_details(result)
+      #return render_template('searchResult.html', name=res)
+      return redirect(url_for('search_result'))
 
 @app.route('/searchResult', methods=['POST','GET'])
 def search_result():
-    return render_template('searchResult.html', name = {})   
+   aname=session['app_name']
+   res,result=scrape3.search(aname)
+   #print res
+   res=scrape3.app_details(result)
+   return render_template('searchResult.html', name =res)   
 
 
-@app.route('/similarApp')
+@app.route('/similarApp', methods=['POST','GET'])
 def appsimilar():
-    return render_template('similar.html')
+   if request.method == 'GET':
+      return render_template('similar.html')
+   elif request.method == 'POST':
+      searchtb= request.form['tb']
+      session['app_name']=searchtb
+   return redirect(url_for('similar_result'))
+
+@app.route('/similarResult', methods=['POST','GET'])
+def similar_result():
+   aname=session['app_name']
+   res,result=scrape3.search(aname)
+   #print res
+   res=scrape3.SimilarApps(result)
+   return render_template('similarResult.html', name =res)   
 
 @app.route('/appCompare')
 def comparing():
